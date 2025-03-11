11b38922a6560081533ac3a4a74122b5c1b5f828
PratamaAngga <anggashillnoir@gmail.com>
2025-03-11 13:28:26 +0700

    percobaan 5.2

---
+++ minggu5/Faktorial20.java
@@ -0,0 +1,21 @@
+package minggu5;
+public class Faktorial20 {
+    int faktorialBF(int n){
+        int faktor = 1;
+        int i=1;
+        while(i<=n){
+            faktor = faktor * i;
+            i++;
+        }
+        return faktor;
+    }
+
+    int faktorialDC(int n){
+        if (n==1) {
+            return 1;
+        } else {
+            int faktor = n * faktorialDC(n-1);
+            return faktor;
+        }
+    }
+}
---
+++ minggu5/MainFaktorial20.java
@@ -0,0 +1,13 @@
+package minggu5;
+import java.util.Scanner;
+public class MainFaktorial20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+        Faktorial20 fk = new Faktorial20();
+        System.out.println("Nilai faktorial "+nilai+" menggunakan BF: "+fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial "+nilai+" menggunakan DC: "+fk.faktorialDC(nilai));
+        input.close();
+    }
+}
